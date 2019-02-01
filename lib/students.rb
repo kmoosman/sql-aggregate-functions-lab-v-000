6dc8791db29e29b21e36458abe8481d22fcd1447
Katie Moosman <moosman.katie@gmail.com>
2019-02-01 04:19:37 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,7 +2,7 @@
 
 def highest_student_gpa
   # "SELECT MAX(gpa) FROM students;"
-  "SELECT * FROM students DESC ORDER BY gpa LIMIT 1"
+  "SELECT * FROM students ORDER BY gpa LIMIT 1"
 end
 
 def lowest_student_gpa
